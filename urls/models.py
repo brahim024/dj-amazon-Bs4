332cc51896c9024fc3369d2a5f3acf40ce69ce33
brahim024 <ifninos168@gmail.com>
2021-02-15 19:15:19 +0100

    verify price old and current

--- urls/models.py
+++ urls/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from .utils import get_urls_data
 # Create your models here.
 class Link(models.Model):
 	name=models.CharField(max_length=300)
@@ -12,3 +12,23 @@ class Link(models.Model):
 
 	def __str__(self):
 		return str(self.name)
+
+	class Meta:
+		ordering=('price_different','-created')
+
+	def save(self,*args,**kwargs):
+		name,price=get_urls_data(self.url)
+
+		if self.current_price :
+			if price !=old_price:
+				diff = price-old_price
+				self.price_different=round(diff,2)
+				self.old_price=old_price
+		else:
+			self.current_price=0
+			self.price_different=0
+
+
+		self.name=name
+		self.current_price=price
+		super().save(*args,**kwargs)
